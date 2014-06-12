9b041afbf90f79bb369efae32b8a55f6c2d406fc
Andreas Drivenes <andreas.drivenes@gmail.com>
2014-06-12 09:53:15 +0200

    Added chartjs to dependencies and added a smooth spider diagram to report view

--- client/views/concept/report_view/report_view.js
+++ client/views/concept/report_view/report_view.js
@@ -26,7 +26,28 @@ Template.ReportView.created = function () {
 };
 
 Template.ReportView.rendered = function () {
+
+  var ctx = document.getElementById("spiderEvaluation").getContext("2d");
+  var chart = new Chart(ctx).Line(data, null);
+
 };
 
 Template.ReportView.destroyed = function () {
 };
+
+
+var data = {
+  labels : ["Produktivitet", "Måloppnåelse", "Virkninger", "Relevans", "Levedyktighet", "Samfunnsøkonomisk lønnsomhet"],
+  datasets : [
+    {
+      fillColor : "rgba(220,220,220,0.5)",
+      strokeColor : "rgba(220,220,220,1)",
+      pointColor : "rgba(220,220,220,1)",
+      pointStrokeColor : "#fff",
+      data : [5,3,2,6,4,5] //just some example data as of now
+
+    }
+
+
+  ]
+}

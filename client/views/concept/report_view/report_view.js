c2d7cece969ec22ea46044b74da882dd8d6ac010
Bjørn Bråthen <bjorbrat88@gmail.com>
2014-06-12 10:08:23 +0200

    Fixes issues with route to report.

--- client/views/concept/report_view/report_view.js
+++ client/views/concept/report_view/report_view.js
@@ -25,29 +25,29 @@ Template.ReportView.helpers({
 Template.ReportView.created = function () {
 };
 
-Template.ReportView.rendered = function () {
+// Template.ReportView.rendered = function () {
 
-  var ctx = document.getElementById("spiderEvaluation").getContext("2d");
-  var chart = new Chart(ctx).Line(data, null);
+//   var ctx = document.getElementById("spiderEvaluation").getContext("2d");
+//   var chart = new Chart(ctx).Line(data, null);
 
-};
+// };
 
-Template.ReportView.destroyed = function () {
-};
-
-
-var data = {
-  labels : ["Produktivitet", "Måloppnåelse", "Virkninger", "Relevans", "Levedyktighet", "Samfunnsøkonomisk lønnsomhet"],
-  datasets : [
-    {
-      fillColor : "rgba(220,220,220,0.5)",
-      strokeColor : "rgba(220,220,220,1)",
-      pointColor : "rgba(220,220,220,1)",
-      pointStrokeColor : "#fff",
-      data : [5,3,2,6,4,5] //just some example data as of now
-
-    }
+// Template.ReportView.destroyed = function () {
+// };
 
 
-  ]
-}
+// var data = {
+//   labels : ["Produktivitet", "Måloppnåelse", "Virkninger", "Relevans", "Levedyktighet", "Samfunnsøkonomisk lønnsomhet"],
+//   datasets : [
+//     {
+//       fillColor : "rgba(220,220,220,0.5)",
+//       strokeColor : "rgba(220,220,220,1)",
+//       pointColor : "rgba(220,220,220,1)",
+//       pointStrokeColor : "#fff",
+//       data : [5,3,2,6,4,5] //just some example data as of now
+
+//     }
+
+
+//   ]
+// }

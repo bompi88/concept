77f5b5c90239da6957b0753f1341071d3f4ca306
Andreas Drivenes <andreas.drivenes@gmail.com>
2014-06-12 09:46:21 +0200

    Added ReportView. This is the main view for looking at reports

---
+++ client/views/concept/report_view/report_view.js
@@ -0,0 +1,32 @@
+/*****************************************************************************/
+/* ReportView: Event Handlers and Helpers */
+/*****************************************************************************/
+Template.ReportView.events({
+  /*
+   * Example: 
+   *  'click .selector': function (e, tmpl) {
+   *
+   *  }
+   */
+});
+
+Template.ReportView.helpers({
+  /*
+   * Example: 
+   *  items: function () {
+   *    return Items.find();
+   *  }
+   */
+});
+
+/*****************************************************************************/
+/* ReportView: Lifecycle Hooks */
+/*****************************************************************************/
+Template.ReportView.created = function () {
+};
+
+Template.ReportView.rendered = function () {
+};
+
+Template.ReportView.destroyed = function () {
+};

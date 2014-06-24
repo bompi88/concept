f072731aa2bc98bdb9f751c458433aa86f2fc67a
Bjørn Bråthen <bjorbrat88@gmail.com>
2014-06-24 09:07:47 +0200

    Fixes problem with content-wrapper and footer.

--- client/views/report_view/report_view.js
+++ client/views/report_view/report_view.js
@@ -1,27 +1,29 @@
 Session.setDefault('TextState','short');
 
 /*****************************************************************************/
-/* ReportView: Event Handlers and Helpers */
+/* ReportView: Event Handlers */
 /*****************************************************************************/
 
 Template.ReportView.events({
-
   'click #text-view-short': function(event, tmpl) {
     Session.set('TextState', 'short');
   },
+
   'click #text-view-long': function(event, tmpl) {
     Session.set('TextState', 'long');
   },
+
   'click .edit-btn': function(event, tmpl) {
     Router.go('/reports/' + this._id + '/edit');
   }
 });
+
+/*****************************************************************************/
+/* ReportView: Helpers */
+/*****************************************************************************/
 
 Template.ReportView.helpers({
   textState: function () {
     return Session.get('TextState');
   }
 });
-
-
-

29919fea28c991333b106adf523426a426d55085
Bjørn Bråthen <bjorbrat88@gmail.com>
2014-06-26 11:22:46 +0200

    Adds fundament to pdf creation.

--- client/views/report_view/report_view.js
+++ client/views/report_view/report_view.js
@@ -13,6 +13,10 @@ Template.ReportView.events({
     Session.set('TextState', 'long');
   },
 
+  'click #export-text': function(event, tmpl) {
+    window.open('/generate-pdf/' + this._id);
+  },
+
   'click .edit-btn': function(event, tmpl) {
     Router.go('/reports/' + this._id + '/edit');
   }
@@ -27,3 +31,29 @@ Template.ReportView.helpers({
     return Session.get('TextState');
   }
 });
+
+window.downloadFile = function(sUrl) {
+ 
+    //If in Chrome or Safari - download via virtual link click
+    if (window.downloadFile.isChrome || window.downloadFile.isSafari) {
+        //Creating new link node.
+        var link = document.createElement('a');
+        link.href = sUrl;
+ 
+        if (link.download !== undefined){
+            //Set HTML5 download attribute. This will prevent file from opening if supported.
+            var fileName = sUrl.substring(sUrl.lastIndexOf('/') + 1, sUrl.length);
+            link.download = fileName;
+        }
+ 
+        //Dispatching click event.
+        if (document.createEvent) {
+            var e = document.createEvent('MouseEvents');
+            e.initEvent('click' ,true ,true);
+            link.dispatchEvent(e);
+            return true;
+        }
+    }
+ 
+    window.open(sUrl);
+}

3a83ee3fc6576bbee4c5480025c2c2be21858722
Andreas Drivenes <andreas.drivenes@gmail.com>
2014-06-29 23:07:51 +0200

    Change to GoogleMaps from Leaflet

--- client/views/map/map.js
+++ client/views/map/map.js
@@ -4,79 +4,145 @@
 
 Template.MapView.rendered = function () {
 
-  L.Icon.Default.imagePath = '/packages/leaflet/images';
-  // create a map in the "map" div, set the view to Trondheim and zoom to get most of Norway
-  var map = L.map('map', {doubleClickZoom: false}).setView([63.43, 10.39], 5);
+  var state = this.data.state;
 
-  //add a map control to search for places from the nominatim database
-  map.addControl( new L.Control.Search({
-    url: 'http://nominatim.openstreetmap.org/search?format=json&q={s}',
-    jsonpParam: 'json_callback',
-    propertyName: 'display_name',
-    propertyLoc: ['lat','lon']
-  }) );
+  GoogleMaps.init(
+    {
+        //'sensor': true, //optional
+        'key': 'AIzaSyDgiWBq5ehQ0Dk8MabpXXaFy7WAACAusBM', //optional
+        'language': 'no' //optional
+    }, 
+      function(){
+        var mapOptions = {
+            zoom: 5
+            //mapTypeId: google.maps.MapTypeId.SATELLITE
+        };
+        map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions); 
+        map.setCenter(new google.maps.LatLng( 63.43, 10.39 ));
+        var markerSize = { x: 22, y: 40 };
 
-  // add an OpenStreetMap tile layer
-  L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-  }).addTo(map);
+        google.maps.Marker.prototype.setLabel = function(label){
+          this.label = new MarkerLabel({
+            map: this.map,
+            marker: this,
+            text: label
+          });
+          this.label.bindTo('position', this, 'position');
+        };
 
+        var MarkerLabel = function(options) {
+          this.setValues(options);
+          this.span = document.createElement('span');
+          this.span.className = 'map-marker-label';
+        };
 
-
-  if(this.data.state === 'reportLocationPicker') {
-    var locationAdded = false;
-
-    map.on('dblclick', function(e) {
-      if(!locationAdded) {
-        var marker = L.marker(e.latlng).addTo(map)
-        .on('click', function(event) {
-          if(locationAdded) {
-            map.removeLayer(marker);
-            locationAdded = false;
+        MarkerLabel.prototype = $.extend(new google.maps.OverlayView(), {
+          onAdd: function() {
+            this.getPanes().overlayImage.appendChild(this.span);
+            var self = this;
+            this.listeners = [google.maps.event.addListener(this, 'position_changed', function() { self.draw();})];
+          },
+          draw: function() {
+            var text = String(this.get('text'));
+            var position = this.getProjection().fromLatLngToDivPixel(this.get('position'));
+            this.span.innerHTML = text;
+            this.span.style.left = (position.x - (markerSize.x / 2)) - (text.length * 3) + 10 + 'px';
+            this.span.style.top = (position.y - markerSize.y + 40) + 'px';
           }
         });
-        locationObject.setCoordinates(e.latlng);
-        locationAdded = true;
+
+        if(state === 'reportLocationPicker') {
+          var locationAdded = false;
+
+          google.maps.event.addListener(map, 'dblclick', function(event) {
+            if(!locationAdded) {
+              var marker = new google.maps.Marker({
+                  position: event.latlng,
+                 map: map
+              });
+              marker.setMap(map);
+
+              google.maps.event.addListener(marker, 'click', function(event) {
+                if(locationAdded) {
+                  marker.setMap(null);
+                  locationAdded = false;
+                  locationObject.setCoordinates({});
+                }
+              });
+
+              locationObject.setCoordinates(e.latlng);
+              locationAdded = true;
+
+
+            }
+          });
+
+          Deps.autorun(function () {
+            var report = Router.getData();
+            if(report && report.project && report.project.location && report.project.location.coordinates && report.project.location.coordinates.lat) {
+              var coords = report.project.location.coordinates;
+              locationObject.setCoordinates(coords);  
+              locationAdded = true
+
+            var marker = new google.maps.Marker({
+                  position: locationObject,
+                map: map
+              });
+              marker.setMap(map);
+              google.maps.event.addListener(marker, 'click', function(event) {
+                if(locationAdded) {
+                  marker.setMap(null);
+                  locationAdded = false;
+                }
+              });
+
+            }
+
+          });
+
+
+
+        }
+
+        else if(state === 'reportLocation') {
+          Deps.autorun(function(){
+
+              var reports = Reports.find({});
+
+              reports.forEach(function (report) {
+
+                report.mapPopup = true;
+
+                if(report && report.project && report.project.name && report.project.location && report.project.location.coordinates && report.project.location.coordinates.lat && report.project.location.coordinates.lng) {
+
+                  var div = document.createElement('div');
+
+                  UI.insert(UI.renderWithData(Template.ProjectInformationBox, report), div);
+
+                  var infowindow = new google.maps.InfoWindow({
+                    content: div.innerHTML
+                  });
+
+                  var marker = new google.maps.Marker({
+                  position: new google.maps.LatLng(report.project.location.coordinates.lat, report.project.location.coordinates.lng),
+                    map: map,
+                    label: report.project.name
+                });
+
+                marker.setMap(map);
+                google.maps.event.addListener(marker, 'click', function() {
+                  infowindow.open(map, marker);
+                })
+              }
+
+              });
+            });
+
+        }
       }
-    });
 
-    Deps.autorun(function () {
-      var report = Router.getData();
-      if(report && report.project && report.project.location && report.project.location.coordinates && report.project.location.coordinates.lat) {
-        var coords = report.project.location.coordinates;
-        locationObject.setCoordinates(coords);  
-        locationAdded = true
 
-        var marker = L.marker([coords.lat, coords.lng]).addTo(map).on('click', function(event) {
-          if(locationAdded) {
-            map.removeLayer(marker);
-            locationAdded = false;
-          }
-
-        });
-
-      }
-
-    });
-  }
-
-  else if(this.data.state === 'reportLocation') {
-    Deps.autorun(function() {
-      var reports = Reports.find({});
-
-      reports.forEach(function (report) {
-        var mapDiv =  L.DomUtil.create("div","lbqs");
-        report.mapPopup = true;
-        UI.insert(UI.renderWithData(Template.ProjectInformationBox, report), mapDiv);
-        if(report && report.project && report.project.location && report.project.location.coordinates && report.project.location.coordinates.lat && report.project.location.coordinates.lng) {
-            // add a marker in the given location, attach some popup content to it 
-            var marker = L.marker([report.project.location.coordinates.lat, report.project.location.coordinates.lng]).bindLabel(report.project.name, {noHide: true}).addTo(map);
-            marker.bindPopup(mapDiv);
-          }
-        });
-    });
-
-  }
+  );
 
 };
 

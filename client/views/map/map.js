be6ba363631527be2f2ed3cd9c78e9456863640e
Bjørn Bråthen <bjorbrat88@gmail.com>
2014-06-30 20:39:17 +0200

    Adds search bar to google maps component.

--- client/views/map/map.js
+++ client/views/map/map.js
@@ -12,7 +12,6 @@ Template.MapView.events({
 
 
 Template.MapView.rendered = function () {
-
   var state = this.data.state;
 
   GoogleMaps.init(
@@ -22,9 +21,11 @@ Template.MapView.rendered = function () {
         'language': 'no' //optional
     }, 
       function(){
+
+        var markers = [];
         var mapOptions = {
-            zoom: 5
-            //mapTypeId: google.maps.MapTypeId.SATELLITE
+            zoom: 5,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
         };
         map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions); 
         map.setCenter(new google.maps.LatLng( 63.43, 10.39 ));
@@ -63,6 +64,59 @@ Template.MapView.rendered = function () {
 
         if(state === 'reportLocationPicker') {
           var locationAdded = false;
+
+        // Create the search box and link it to the UI element.
+        var input = /** @type {HTMLInputElement} */(
+            document.getElementById('pac-input'));
+        map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+
+        var searchBox = new google.maps.places.SearchBox(
+          /** @type {HTMLInputElement} */(input));
+
+        // Listen for the event fired when the user selects an item from the
+        // pick list. Retrieve the matching places for that item.
+        google.maps.event.addListener(searchBox, 'places_changed', function() {
+          var places = searchBox.getPlaces();
+
+          if (places.length == 0) {
+            return;
+          }
+          for (var i = 0, marker; marker = markers[i]; i++) {
+            marker.setMap(null);
+          }
+
+          // For each place, get the icon, place name, and location.
+          markers = [];
+          var bounds = new google.maps.LatLngBounds();
+          for (var i = 0, place; place = places[i]; i++) {
+            var image = {
+              url: place.icon,
+              size: new google.maps.Size(71, 71),
+              origin: new google.maps.Point(0, 0),
+              anchor: new google.maps.Point(17, 34),
+              scaledSize: new google.maps.Size(25, 25)
+            };
+
+            // Create a marker for each place.
+            var marker = new google.maps.Marker({
+              map: map,
+              icon: image,
+              title: place.name,
+              position: place.geometry.location
+            });
+
+            markers.push(marker);
+
+            bounds.extend(place.geometry.location);
+          }
+
+          map.fitBounds(bounds);
+        });
+
+        google.maps.event.addListener(map, 'bounds_changed', function() {
+          var bounds = map.getBounds();
+          searchBox.setBounds(bounds);
+        });
 
           google.maps.event.addListener(map, 'click', function(e) {
             if(!locationAdded) {

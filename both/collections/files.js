b0ea5ef67a111f43e82078c2650e9c203b091992
Bjørn Bråthen <bjorbrat88@gmail.com>
2014-06-30 22:21:00 +0200

    Restrict upload to office documents (MS Office and iWork) and .pdf files.

--- both/collections/files.js
+++ both/collections/files.js
@@ -3,5 +3,23 @@
 /*****************************************************************************/
 
 Files = new FS.Collection("files", {
-  stores: [ new FS.Store.GridFS("files", {})]
+	stores: [
+		new FS.Store.GridFS("files", {})
+	],
+	filter: {
+		allow: {
+			contentTypes: [
+				'application/pdf',
+				'application/vnd.openxmlformats-officedocument.wordprocessingml.document',
+				'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+				'application/vnd.openxmlformats-officedocument.presentationml.presentation',
+				'application/msword',
+				'application/vnd.ms-excel',
+				'application/vnd.ms-powerpoint',
+				'application/x-iwork-keynote-sffkey',
+				'application/x-iwork-pages-sffpages',
+				'application/x-iwork-numbers-sffnumbers'
+			]
+		}
+	}
 });

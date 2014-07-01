b72adb5474cc3ffa0abec1fc094a7ebe05493df8
Bjørn Bråthen <bjorbrat88@gmail.com>
2014-07-01 14:38:41 +0200

    Aesthetic code changes.

--- client/config/startup.js
+++ client/config/startup.js
@@ -1,31 +1,19 @@
+/**
+ * Startup configurations
+ */
 Meteor.startup(function() {
-	//specify the site language
+
+	// Specify the site language
 	$('html').attr('lang', 'no-nb');
 
-	//display a warning if the user has an outdated browser
-	var $buoop = {}; 
-	$buoop.ol = window.onload; 
-	window.onload=function(){ 
-		try {
-			if ($buoop.ol) 
-				$buoop.ol();
-		}
-		catch (e) {} 
-		var e = document.createElement("script"); 
-		e.setAttribute("type", "text/javascript"); 
-		e.setAttribute("src", "//browser-update.org/update.js"); 
-		document.body.appendChild(e); 
-	}
+  // Set the internationalization
+  i18n.setLanguage('nb_NO');
 
-	//dirty hack for dismissing modal by clicking outside it. so we dont have to fork bootbox
+	// Dirty hack for dismissing modal by clicking outside it. so we dont have to fork bootbox
 	$(document).on('click', '.bootbox', function(){
     var classname = event.target.className;
 
     if(classname && !$('.' + classname).parents('.modal-dialog').length)
-        bootbox.hideAll();
+      bootbox.hideAll();
 	});
-
-
-
-i18n.setLanguage('nb_NO');
 });

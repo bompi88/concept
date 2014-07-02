624933ab36341aa3eaecf88a0c03023bd4bce5c6
Bjørn Bråthen <bjorbrat88@gmail.com>
2014-07-02 12:40:07 +0200

    Indenting etc.. for client collection js files.

--- both/collections/images.js
+++ both/collections/images.js
@@ -1,11 +1,11 @@
-/*****************************************************************************/
-/* Images Collection */
-/*****************************************************************************/
+/**
+ * Images Collection
+ */
 
 Images = new FS.Collection("images", {
   stores: [
   new FS.Store.GridFS("original", {
-    transformWrite: function(fileObj, readStream, writeStream) { 
+    transformWrite: function(fileObj, readStream, writeStream) {
         // Transform the image into a smaller image,
         // max 1024 pixels in either direction
         gm(readStream, fileObj.name).resize('1024', '1024')
@@ -13,7 +13,7 @@ Images = new FS.Collection("images", {
       }
     }),
   new FS.Store.GridFS("thumbs", {
-    transformWrite: function(fileObj, readStream, writeStream) { 
+    transformWrite: function(fileObj, readStream, writeStream) {
         // Transform the image into a thumbnail
         gm(readStream, fileObj.name).resize('200', '200')
           .setFormat("PNG").stream().pipe(writeStream);

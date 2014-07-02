e76915cc9eef89e7d2022edbd82d7cc58a06c6a4
Bjørn Bråthen <bjorbrat88@gmail.com>
2014-07-02 12:44:46 +0200

    Refactoring of client/views/concept_index/*

--- client/views/concept_index/concept_index.js
+++ client/views/concept_index/concept_index.js
@@ -1,36 +1,6 @@
-/*****************************************************************************/
-/* ConceptIndex: Event Handlers and Helpers */
-/*****************************************************************************/
-Template.ConceptIndex.events({
-  /*
-   * Example: 
-   *  'click .selector': function (e, tmpl) {
-   *
-   *  }
-   */
-});
-
-Template.ConceptIndex.helpers({
-  /*
-   * Example: 
-   *  items: function () {
-   *    return Items.find();
-   *  }
-   */
-});
-
-/*****************************************************************************/
-/* ConceptIndex: Lifecycle Hooks */
-/*****************************************************************************/
-Template.ConceptIndex.created = function () {
-};
-
 Template.ConceptIndex.rendered = function () {
   Hyphenator.config({
     minwordlength : 4
   });
   Hyphenator.run();
-};
-
-Template.ConceptIndex.destroyed = function () {
 };

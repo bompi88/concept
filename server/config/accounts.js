6593a2273b309e86117c2aad01ac5c7cff050516
Bjørn Bråthen <bjorbrat88@gmail.com>
2014-07-24 11:17:16 +0200

    Increases the grid break point and removes some text in emails.

--- server/config/accounts.js
+++ server/config/accounts.js
@@ -22,7 +22,7 @@ Meteor.startup(function() {
 Accounts.emailTemplates.siteName = "Concepts presentasjonsverktøy";
 Accounts.emailTemplates.from = "Concept <accounts@concept.ntnu.no>";
 Accounts.emailTemplates.enrollAccount.subject = function (user) {
-  return "Velkommen gamle sjel!";
+  return "Velkommen til Concepts presentasjonsverktøy!";
 };
 Accounts.emailTemplates.enrollAccount.text = function (user, url) {
   return "Du er så heldig, og har nå fått superbrukerkonto i Concepts sitt presentasjonsvektøy!"
@@ -35,7 +35,6 @@ Accounts.emailTemplates.resetPassword.subject = function (user) {
 };
 Accounts.emailTemplates.resetPassword.html = function (user, url) {
   return "Hver og en kan glemme passordet sitt i blant."
-    + " Hvis dette intrer ofte, kan det være til hjelp å ta en titt <a href='http://www.nyttig.net/helse/hukommelse'>her</a> eller <a href='http://home.online.no/~harc/trimming/'>her</a>.<br><br>"
     + " For å resette passordet, klikker du på linken under:<br><br>"
     + url;
 };

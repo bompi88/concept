811270ff3625832dd43c92b0b4bdf42b5e980343
Andreas Drivenes <andreas.drivenes@gmail.com>
2014-09-05 11:31:17 +0200

    Remove text hyphenating

--- client/views/reports/views/box/basic_box/basic_box.js
+++ client/views/reports/views/box/basic_box/basic_box.js
@@ -7,21 +7,3 @@ Template.BasicBox.events({
     Router.go(Router.path('Report', {_id: this._id}));
   }
 });
-
-
-var elements;
-
-Template.BasicBox.rendered = function () {
-  elements = $('.hyphenate').hyphenate('no-nb');
-};
-
-Template.BasicBox.destroyed = function () {
-  if(elements) {
-    for (var i = 0, element; element = elements[i]; i++) {
-      element = null;
-      delete element;
-    }
-  }
-
-  elements = null;
-};

755126f92ea2620bb6a03f0d4d957bb595bcfb2e
Andreas Drivenes <andreas.drivenes@gmail.com>
2014-10-04 23:32:48 +0200

    No need for setting i18n language in startup anymore

--- client/config/startup.js
+++ client/config/startup.js
@@ -6,9 +6,6 @@ Meteor.startup(function() {
 	// Specify the site language
 	$('html').attr('lang', 'no-nb');
 
-  // Set the internationalization
-  i18n.setLanguage('nb');
-
 	// Dirty hack for dismissing modal by clicking outside it. so we dont have to fork bootbox
 	$(document).on('click', '.bootbox', function(){
     var classname = event.target.className;

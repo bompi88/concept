4671ed38f6a6b5dc7637507f826d711143eeb21b
Bjørn Bråthen <bjorbrat88@gmail.com>
2015-03-12 20:09:09 +0100

    You cannot delete the last super user.

--- client/views/accounts/user_control/user_control.js
+++ client/views/accounts/user_control/user_control.js
@@ -1,13 +1,65 @@
 Template.UserControl.events({
+
   'click #rem-user': function(event) {
     event.preventDefault();
-    Meteor.call('deleteConceptUser', this._id, Meteor.userId(), function(error, result) {
-      if(error)
-        bootbox.warn(error);
-      else
-        bootbox.alert('Bruker slettet!');
-    });
+    var that = this;
+
+    // A confirmation prompt before removing the document
+    var confirmationPrompt = {
+      title: "Bekreftelse på slettingen",
+      message: 'Er du sikker på at du vil slette brukeren?',
+      buttons: {
+        cancel: {
+          label: "Nei"
+        },
+        confirm: {
+          label: "Ja",
+          callback: function(result) {
+            if(result) {
+              // Remove the document
+              Meteor.call('deleteConceptUser', that._id, Meteor.userId(), function(error, result) {
+                if(error)
+                  bootbox.alert(error);
+                else
+                  bootbox.alert('Bruker slettet!');
+              });
+            }
+          }
+        }
+      }
+    }
+    bootbox.dialog(confirmationPrompt);
   },
+
+  'click #set-superuser': function(event, tmpl) {
+    var that = this;
+
+    // A confirmation prompt before removing the document
+    var confirmationPrompt = {
+      title: "Bekreftelse på endringen av rettigheter",
+      message: 'Er du sikker på at du vil gi ' + (this.emails && this.emails[0] && this.emails[0].address || 'denne brukeren') + ' superbruker rettigheter?',
+      buttons: {
+        cancel: {
+          label: "Nei"
+        },
+        confirm: {
+          label: "Ja",
+          callback: function(result) {
+            if(result) {
+             Meteor.call('toggleSuperuser', that._id, !isSuperUser(that._id), function(error, result) {
+                if(error)
+                  bootbox.alert(error);
+                else
+                  bootbox.alert('Brukerrettigheter ble endret!');
+              });
+            }
+          }
+        }
+      }
+    }
+    bootbox.dialog(confirmationPrompt);
+  },
+
   'submit form': function(event, tmpl) {
     event.preventDefault();
     try {
@@ -27,9 +79,11 @@ Template.UserControl.events({
       bootbox.alert('Noe gikk galt. Prøv på nytt');
     }
   }
+
 });
 
 Template.UserControl.helpers({
+
   status: function() {
     var status = "";
     if(this.emails && this.emails[0] && this.emails[0] && this.emails[0].verified)
@@ -42,10 +96,23 @@ Template.UserControl.helpers({
 
     return status;
   },
-  isSuperUser: function() {
+
+  isSuperUser: function(id) {
+    return isSuperUser(id);
+  }
+
+});
+
+var isSuperUser = function(id) {
+  if(id) {
+    var user = Meteor.users.findOne({_id: id});
+    if(user && user.accountType && user.accountType === 'admin') {
+      return true;
+    }
+  } else {
     if(Meteor.user() && Meteor.user().accountType && Meteor.user().accountType === 'admin') {
       return true;
     }
-    return false;
   }
-});
+  return false;
+}
--- server/methods.js
+++ server/methods.js
@@ -4,6 +4,7 @@
 "use strict";
 
 Meteor.methods({
+
 	toggleReportPublic: function(id, publicity) {
 		if(this.userId) {
 			return Reports.update({_id: id}, {$set: {"_public": publicity}});
@@ -11,9 +12,11 @@ Meteor.methods({
 			return false;
 		}
 	},
+
   totalCount: function(query) {
     return Reports.find(query).count();
   },
+
   createConceptUser: function(newUserEmail, userId) {
     var user = Meteor.users.findOne({_id: userId});
     if(user && user.accountType && user.accountType === 'admin') {
@@ -29,13 +32,52 @@ Meteor.methods({
     else
       throw new Meteor.Error(404, 'User is not a super user');
   },
+
   deleteConceptUser: function(userIdToBeDeleted, userId) {
     var user = Meteor.users.findOne({_id: userId});
     if(user && user.accountType && user.accountType === 'admin') {
-      Meteor.users.remove(userIdToBeDeleted);
+      if(Meteor.users.find().count() > 1) {
+        if(userIdToBeDeleted == userId) {
+          if(Meteor.users.find({ accountType: 'admin' }).count() == 1) {
+            throw new Meteor.Error(404, 'Du kan ikke slette den siste superbrukeren');
+          } else {
+            Meteor.users.remove(userIdToBeDeleted);
+          }
+        } else {
+          Meteor.users.remove(userIdToBeDeleted);
+        }
+      } else {
+        throw new Meteor.Error(404, 'Du kan ikke slette den siste superbrukeren');
+      }
     }
     else
       throw new Meteor.Error(404, 'User is not a super user');
+  },
 
+  toggleSuperuser: function(id, give) {
+    var user = Meteor.users.findOne({_id: this.userId});
+
+    if(user && user.accountType && user.accountType === 'admin') {
+      if(give)
+        Meteor.users.update({_id: id}, {$set : {accountType: 'admin'}});
+      else {
+        if(Meteor.users.find().count() > 1) {
+          if(id == this.userId) {
+            if(Meteor.users.find({ accountType: 'admin' }).count() == 1) {
+              throw new Meteor.Error(404, 'Du kan ikke frata rettigheter på den siste superbrukeren');
+            } else {
+              Meteor.users.update({_id: id}, {$set : {accountType: ''}});
+            }
+          } else {
+            Meteor.users.update({_id: id}, {$set : {accountType: ''}});
+          }
+        } else {
+          throw new Meteor.Error(404, 'Du kan ikke frata rettigheter på den siste superbrukeren');
+        }
+      }
+    } else {
+      throw new Meteor.Error(404, 'Du har ikke rettigheter til å gjøre dette');
+    }
   }
+
 });
